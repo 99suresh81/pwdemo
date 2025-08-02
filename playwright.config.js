70306b76630cf4830dc1071f8e127462cea8271d
99suresh81 <60417781+99suresh81@users.noreply.github.com>
2025-08-02 12:07:57 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
-    screenshot: 'only-on-failure',
+    screenshot: 'on', // 'only-on-failure'
     video: 'retain-on-failure',
   },
 });
